d033d50e2a5a5c4e6c04c7121f8fc40ccf63829d
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-14 08:12:03 -0800

    feat:iteration 1

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,21 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
 
+  def initialize(name, height, money)
+    @name           = name
+    @height         = height
+    @spending_money = money.delete(money[0]).to_i
+    @preferences    = []
+  end
+
+  def add_preferences(preference)
+    preferences.push(preference)
+  end
+
+  def tall_enough(height)
+    @height >= height
+  end
 end
